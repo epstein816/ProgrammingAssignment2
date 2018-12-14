f50f685ef61d0f1217dbbf5cea3ca86782c9fd39
epstein816 <luepstein@ucdavis.edu>
2018-12-14 11:29:41 -0800

    3rd attempt to commit pa2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## creates a list of functions to 1) set value of matrix 2) get value of matrix 
+#  3) set value of inverse matrix 4) get value of inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(solve) inv <<- solve
+  getinv <- function() inv
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## returns cached value of inverse matrix
+## will not function without being called in makecachematrix environment
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinv(inv)
+  inv      
 }
+
+
+
+## the following will test the functions created
+b <- matrix(1:4, 2, 2)
+c <- makeCacheMatrix(b)
+cacheSolve(c)
+
+## Should result in:
+##     [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
+
